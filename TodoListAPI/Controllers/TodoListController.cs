967820b68659e0c44218c650a44f125840d34432
praveen <praveendarshana.slic@gmail.com>
2024-11-16 15:10:10 +0530

    first commit

---
+++ TodoListAPI/Controllers/TodoListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListAPI.Controllers
+{
+    public class TodoListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
