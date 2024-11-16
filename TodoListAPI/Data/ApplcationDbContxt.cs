966ed8ff759e923908d81d1ca9aa912f4941ccd9
praveen <praveendarshana.slic@gmail.com>
2024-11-16 14:52:34 +0530

    Add project files.

---
+++ TodoListAPI/Data/ApplcationDbContxt.cs
@@ -0,0 +1,12 @@
+﻿using TodoListAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoListAPI.Data
+{
+    public class ApplcationDbContxt : DbContext
+    {
+        public ApplcationDbContxt(DbContextOptions options): base(options) { }
+
+        public DbSet<TodoModel> Todos { get; set; }
+    }
+}
---
+++ TodoListAPI/Models/Entities/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TodoListAPI.Models.Entities
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
